274673632d56ba18fd45cedd11a059da0c606f5b
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-21 02:00:48 +0500

    task one completed

---
+++ app.js
@@ -0,0 +1,20 @@
+var screenDiV = document.getElementById('imagesDiv')
+var images = screenDiV.getElementsByTagName('img')
+var sources = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpeg","7.png","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg",]
+for (var i = 0; i < images.length ; i++){
+   images[i].src = sources[i]
+}
+function showModel(image){
+  var model = document.getElementById('modal')
+  model.classList.add('modal-open')
+  model.classList.remove('modal-close')
+  model.style.display = 'block'
+  document.getElementById("modal-img").src = image.src;
+}
+function onClosedImagModal(){
+    var model = document.getElementById('modal')
+    model.classList.remove('modal-open')
+    model.classList.add('modal-close')
+    model.style.display = 'block'
+    setTimeout(()=>{ modal.style.display = "none"; }, 550)
+  }
